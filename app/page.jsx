fd3eec0acf8fd0260a4579270581b3280a6751c6
admin <admin@joe.local>
2024-05-21 20:23:04 +0500

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Feed from "@components/Feed";
+
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      Discover & Share
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> Useful Tips</span>
+    </h1>
+    <p className='desc text-center'>
+      ProTips is a Tips and hacks platform to
+      discover, create and share creative Tips on any subject!
+    </p>
+
+    <Feed />
+  </section>
+);
+
+export default Home;
